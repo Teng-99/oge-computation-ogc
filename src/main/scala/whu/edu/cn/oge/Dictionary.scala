a4dc70d2d327377edc81cb05b89de8838c1c2b49
Teng-99 <44767543+Teng-99@users.noreply.github.com>
2023-08-15 10:11:35 +0800

    Init.

---
+++ src/main/scala/whu/edu/cn/oge/Dictionary.scala
@@ -0,0 +1,7 @@
+package whu.edu.cn.oge
+
+object Dictionary {
+  def main(args: Array[String]):Unit={
+
+  }
+}
---
+++ src/main/scala/whu/edu/cn/oge/Vector.scala
@@ -0,0 +1,7 @@
+package whu.edu.cn.oge
+
+object Vector {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
